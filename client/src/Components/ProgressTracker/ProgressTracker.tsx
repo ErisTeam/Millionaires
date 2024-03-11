bb71757b7184cd1420bd07c7d30523d50eb513e5
Gami <gamiofficial0@gmail.com>
2024-03-11 12:57:14 +0100

    fixes and more fixes

--- client/src/Components/ProgressTracker/ProgressTracker.tsx
+++ client/src/Components/ProgressTracker/ProgressTracker.tsx
@@ -1,58 +1,70 @@
 import {
-	IconDiamondFilled,
 	IconDiamonds,
 	IconDiamondsFilled,
 	IconPhoneCall,
-	IconRobot,
 	IconShieldHalfFilled,
 	IconUsersGroup,
 } from '@tabler/icons-solidjs';
 import style from './ProgressTracker.module.css';
-import { For, createSignal } from 'solid-js';
+import { For, Show } from 'solid-js';
 import Hexagon from '../Hexagon/Hexagon';
 import { useAppState } from '@/AppState';
-//TODO: make this update on question change
 export default function ProgressTracker() {
 	const AppState = useAppState();
 
 	console.log(AppState.questionsStatus());
 	return (
 		<div class={style.ladder}>
-			<span class={style.lifeLinesContainer}>
-				<IconUsersGroup class={style.lifeLine} />
-				<button></button>
-				<IconShieldHalfFilled
-					class={style.lifeLine}
-					classList={{
-						[style.used]: true,
-					}}
-				/>
-				<IconPhoneCall class={style.lifeLine} />
-			</span>
+			<section class={style.lifeLinesContainer}>
+				<button class={style.lifeLine}>
+					<IconUsersGroup />
+				</button>
+				<button class={style.lifeLine}>
+					<IconShieldHalfFilled />
+				</button>
+				<button class={style.lifeLine}>
+					<IconPhoneCall />
+				</button>
+			</section>
 			<ol>
 				<For each={AppState.questionsStatus()}>
-					{(v, index) => {
-						const currentQuestion =
-							(index() == 0 && !v.answered) || (AppState.questionsStatus()[index() - 1]?.answered && !v.answered);
-						return (
+					{(v, index) => (
+						<Show
+							when={
+								(index() == 0 && !AppState.questionsStatus()[0].answered) ||
+								(AppState.questionsStatus()[index() - 1]?.answered && !v.answered)
+							}
+							fallback={
+								<li
+									class={style.question}
+									classList={{
+										[style.answered]: v.answered,
+									}}
+								>
+									<Hexagon class={style.hexagon} />
+									{index() + 1}
+									<IconDiamondsFilled class={style.diamond + ' ' + style.diamondFilled} />
+
+									<span>{v.value}</span>
+								</li>
+							}
+						>
 							<li
 								class={style.question}
 								classList={{
 									[style.answered]: v.answered,
-									[style.currentQuestion]: currentQuestion,
+									[style.currentQuestion]: true,
 								}}
 							>
 								<Hexagon class={style.hexagon} />
 								{index() + 1}
-								{currentQuestion ? (
-									<IconDiamonds class={style.diamond} />
-								) : (
-									<IconDiamondsFilled class={style.diamond + ' ' + style.diamondFilled} />
-								)}
+
+								<IconDiamonds class={style.diamond} />
+
 								<span>{v.value}</span>
 							</li>
-						);
-					}}
+						</Show>
+					)}
 				</For>
 			</ol>
 		</div>
--- client/src/Pages/Game/Game.tsx
+++ client/src/Pages/Game/Game.tsx
@@ -2,11 +2,8 @@ import 'solid-devtools';
 import style from './Game.module.css';
 import ProgressTracker from '@/Components/ProgressTracker/ProgressTracker';
 import AnswerButton from '@/Components/AnswerButton/AnswerButton';
-import ExampleAnswers from '@/TestData/Answer';
 import Question from '@/Components/Question/Question';
-import LifeLine1 from '@/Components/LifeLine1/LifeLine1';
-import ConfirmationModal from '@/Components/ConfirmationModal/ConfirmationModal';
-import { For, Show, onMount } from 'solid-js';
+import { For, Show } from 'solid-js';
 import { useNavigate } from '@solidjs/router';
 import { useAppState } from '@/AppState';
 import { Question as QuestionT } from '@/protobufMessages/Questions';
@@ -21,13 +18,48 @@ export default function Game() {
 	if (AppState.runID() == undefined || AppState.username() == undefined || AppState.currentQuestion() == undefined) {
 		shouldShow = false;
 		navigate('/');
-		alert('Nie masz prawa tu być');
+		// alert('Nie masz prawa tu być');
+	}
+	function handleAnwerClick(answerId: number) {
+		let runId = AppState.runID();
+		if (runId == undefined) {
+			// alert('Nie masz prawa tu być');
+			navigate('/');
+			return;
+		}
+		answerQuestion(runId, answerId).then((x) => {
+			console.log(x);
+			if (x.isCorrect) {
+				// alert('Dobrze');
+				if (x.nextQuestion == undefined) {
+					alert('Koniec');
+					navigate('/results');
+					return;
+				}
+				AppState.setQuestionsStatus((prev) => {
+					let f = prev.findIndex((v) => v.answered == false);
+					if (f != undefined) {
+						let newState = [...prev];
+						newState[f].answered = true;
+
+						return newState;
+					}
+
+					return prev;
+				});
+				console.log(AppState.questionsStatus());
+				AppState.setCurrentQuestion(x.nextQuestion);
+			} else {
+				alert('Zle');
+				navigate('/results');
+			}
+		});
 	}
 
 	return (
 		<Show when={shouldShow}>
 			<div class={style.container}>
-				<div class={style.game}>
+				<main class={style.game}>
 					{/* <div class={style.ai}>
 					<div class={style.questionImg}></div>
 					<div class={style.host}></div>
@@ -42,37 +74,8 @@ export default function Game() {
 											<AnswerButton
 												onClick={(_) => {
 													//enable animation here
-													let runId = AppState.runID();
-													if (runId == undefined) {
-														alert('Nie masz prawa tu być');
-														navigate('/');
-														return;
-													}
-													answerQuestion(runId, answer.id).then((x) => {
-														console.log(x);
-														if (x.isCorrect) {
-															alert('Dobrze');
-															if (x.nextQuestion == undefined) {
-																alert('Koniec');
-																navigate('/results');
-																return;
-															}
-															AppState.setQuestionsStatus((prev) => {
-																let f = prev.findIndex((v) => v.answered == false);
-																if (f != undefined) {
-																	let newState = [...prev];
-																	newState[f].answered = true;
-																	return newState;
-																}
-																return prev;
-															});
-															console.log(AppState.questionsStatus());
-															AppState.setCurrentQuestion(x.nextQuestion);
-														} else {
-															alert('Zle');
-															navigate('/results');
-														}
-													});
+
+													handleAnwerClick(answer.id);
 												}}
 												answer={answer}
 											/>
@@ -82,8 +85,9 @@ export default function Game() {
 							</For>
 						</ol>
 					</div>
-				</div>
-				<ProgressTracker class={style.progressTracker} />
+				</main>
+				<ProgressTracker />
+				{/* <ProgressTracker class={style.progressTracker} /> */}
 				{/* <ConfirmationModal /> */}
 				{/* <LifeLine1 /> */}
 			</div>
